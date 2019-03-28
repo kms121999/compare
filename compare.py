8b32bf0df2d0f63a370ad38847e6f7ee8a9cc4c4
Keaton Smith <smithkeaton12@gmail.com>
2019-03-28 17:50:17 -0500

    Added the initial compare.py file

---
+++ compare.py
@@ -0,0 +1,32 @@
+# User prompts
+NUM1_PROMPT = "Please, enter the first number: "
+NUM2_PROMPT = "Please, enter the second number: "
+
+def main():
+    # Get the first number
+    firstNumber = getInt(NUM1_PROMPT)
+    # Get the second number
+    secondNumber = getInt(NUM2_PROMPT)
+
+    # Compare the numbers
+    if firstNumber > secondNumber:
+        print("num 1 is bigger than num 2")
+
+
+def getInt(prompt):
+    # Initialize variables
+    userInput = ''
+
+    # While the no valid user input
+    while userInput == '':
+        try:
+            # Prompt for input and attempt to convert to int
+            userInput = int(input(prompt))
+        except ValueError:
+            pass
+
+    # Return the user's input in int form
+    return userInput
+
+# Call main
+main()
